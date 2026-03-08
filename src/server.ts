14b961c955005cbdc7c8bde3c20b5d1fa3efff04
fipestar <barbosaneira@gmail.com>
2026-03-08 17:35:22 -0500

    Cors Options

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,9 @@ import swaggerUi from 'swagger-ui-express'
 import swaggerSpec from "./config/swagger";
 import router from "./router";
 import db from "./config/db";
+import { CorsOptions } from "cors";
+import cors from "cors";
+import morgan from "morgan";
 
 //Conectar a la db
 export async function conectDB(){
@@ -18,7 +21,21 @@ export async function conectDB(){
 
 const server = express();
 
+//Permitir conexiones
+const corsOptions: CorsOptions = {
+    origin: function (origin, callback) {
+        if(origin === process.env.FRONTEND_URL) {
+            callback(null, true)
+        } else {
+            callback(new Error('No permitido por CORS'))
+        }
+    }
+}
+server.use(cors(corsOptions))
+
 server.use(express.json())
+
+server.use(morgan('dev'))
 
 server.get('/api', (req, res) => {
     res.json({ msg: 'Desde API' })
